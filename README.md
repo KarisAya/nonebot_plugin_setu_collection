71deb6c5cc9da2522e95fd2d68017e9209273fc1
KarisAya <1048827424@qq.com>
2024-04-26 18:51:16 +0800

    clovers 插件化

--- README.md
+++ README.md
@@ -3,37 +3,8 @@
 </p>
 <div align="center">
 
-# nonebot_plugin_setu_collection
+# nonebot-plugin-setu-collection
 
 </div>
 
-[Jitsu](https://image.anosu.top/) 简单好用的色图api
-
-[MirlKoi API](https://iw233.cn/) 可以避免bot被封的超安全（保证好看但是不怎么涩）色图api
-
-[Lolicon API](https://api.lolicon.app/) 使用这个的时候请注意身体
-
-## 安装
-    pip install nonebot_plugin_setu_collection
-## 使用
-    nonebot.load_plugin('nonebot_plugin_setu_collection')
-
-## 介绍
-
-__指令__：`来N张xx色图` `来N张xx` `来N张r18xx色图`
-
-__指令（仅私聊）__：切换api
-
-仅私聊允许r18色图
-
-单次最多发送10张色图
-
-群聊支持单个tag
-
-私聊支持3个tag,用空格隔开。
-    
-## 其他
-
-如有建议，bug反馈，以及讨论新玩法，新机制（或者单纯没有明白怎么用）可以来加群哦~
-
-![群号](https://github.com/KarisAya/nonebot_plugin_game_collection/blob/master/%E9%99%84%E4%BB%B6/qrcode_1665028285876.jpg)
+[插件说明](https://github.com/clovers-project/clovers-setu-collection)
--- clovers_setu_collection/__init__.py
+++ clovers_setu_collection/__init__.py
@@ -1,77 +1,27 @@
-import re
-from clovers.core.plugin import Plugin, Event, Result
-from clovers.utils.tools import to_int, download_url
-from .api.MirlKoi import MirlKoi, is_MirlKoi_tag
-from .api.Anosu import Anosu
+from clovers.core.plugin import PluginLoader
+from nonebot_plugin_clovers import clovers
+from nonebot.log import logger
+from nonebot.plugin import PluginMetadata
 
-plugin = Plugin()
-
-
-@plugin.handle({"涩图，色图"}, ["to_me"])
-async def _(event: Event):
-    if not event.kwargs["to_me"]:
-        return
-    msg = (
-        "发送【我要一张xx涩图】可获得一张随机色图。"
-        "群聊图片取自：\n"
-        "Jitsu：https://image.anosu.top/\n"
-        "MirlKoi API：https://iw233.cn/\n"
-        "私聊图片取自：\n"
-        "Lolicon API：https://api.lolicon.app/"
-    )
-    return Result("text", msg)
+__plugin_meta__ = PluginMetadata(
+    name="来张色图",
+    description="从多个api获取色图并根据场景整合的色图插件",
+    usage="来N张xx色图",
+    type="application",
+    homepage="https://github.com/KarisAya/nonebot_plugin_setu_collection",
+    supported_adapters={"nonebot.adapters.onebot.v11"},
+)
 
 
-@plugin.handle(r"来(.*)[张份]([rR]18)?(.+)$", ["Bot_Nickname", "group_id", "user_id"])
-async def _(event: Event) -> Result:
-    Bot_Nickname = event.kwargs["Bot_Nickname"]
-    n, r18, tag = event.args
-    if n:
-        n = to_int(n)
-        if n is None:
-            return
+def load_plugin(name: str):
+    plugin = PluginLoader.load(name)
+    if plugin is None:
+        logger.error(f"未找到{name}")
+    elif plugin not in clovers.plugins:
+        clovers.plugins.append(plugin)
+        logger.success(f"{name}加载成功")
     else:
-        n = 1
-    if tag[-2:] in {"色图", "涩图", "图片"}:
-        tag = tag[:-2]
-    resp = []
-    if n > 5:
-        n = 5
-        resp.append("最多可以点5张图片哦")
-
-    resp.append(f"{Bot_Nickname}为你准备了{n}张随机{tag}图片！")
-
-    def choice_api(tag: str):
-        if not tag or (tag := is_MirlKoi_tag(tag)):
-            api = "MirlKoi API"
-            setufunc = MirlKoi
-        else:
-            api = "Jitsu"
-            setufunc = Anosu
-        return setufunc
-
-    if event.kwargs["group_id"]:
-        if r18:
-            resp.append("(r18禁止)")
-
-    else:
-        if r18:
-            r18 = 1
-
-    msg += f"{Bot_Nickname}为你准备了{N}张随机{Tag or '图片'}！\n图片取自：{api}"
-    url_list = await setufunc(N, Tag)
-    if url_list is None:
-        return Result("text", msg + "\n连接失败，请稍等一年后重试。")
-    image_list = [Result("image", url) for url in url_list if url]
-    if len(image_list) == 1:
-        return Result("list", [Result("text", msg), image_list[0]])
-
-    async def result():
-        yield Result("text", msg)
-        for x in image_list:
-            yield x
-
-    return Result("segmented", result())
+        logger.success(f"{name}已存在")
 
 
-__plugin__ = plugin
+load_plugin("clovers_setu_collection")
