c1a47bc9fc0342923d1f197116827d7558cc9345
KarisAya <1048827424@qq.com>
2024-04-27 16:02:00 +0800

    Updating clovers (0.1.5 -> 0.1.7)

--- README.md
+++ README.md
@@ -5,6 +5,17 @@
 
 # nonebot-plugin-setu-collection
 
+<img src="https://img.shields.io/badge/python-3.12+-blue.svg" alt="python">
+<a href="./LICENSE">
+  <img src="https://img.shields.io/github/license/KarisAya/nonebot_plugin_setu_collection.svg" alt="license">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot_plugin_setu_collection">
+  <img src="https://img.shields.io/pypi/v/nonebot_plugin_setu_collection.svg" alt="pypi">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot_plugin_setu_collection">
+  <img src="https://img.shields.io/pypi/dm/nonebot_plugin_setu_collection" alt="pypi download">
+</a>
+
 </div>
 
 [插件说明](https://github.com/clovers-project/clovers-setu-collection)
--- nonebot_plugin_setu_collection/__init__.py
+++ nonebot_plugin_setu_collection/__init__.py
@@ -1,8 +1,5 @@
-from clovers.core.plugin import PluginLoader
-from nonebot_plugin_clovers import clovers
-from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
-
+from nonebot_plugin_clovers import clovers
 __plugin_meta__ = PluginMetadata(
     name="来张色图",
     description="从多个api获取色图并根据场景整合的色图插件",
@@ -11,17 +8,4 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/KarisAya/nonebot_plugin_setu_collection",
     supported_adapters={"nonebot.adapters.onebot.v11"},
 )
-
-
-def load_plugin(name: str):
-    plugin = PluginLoader.load(name)
-    if plugin is None:
-        logger.error(f"未找到{name}")
-    elif plugin not in clovers.plugins:
-        clovers.plugins.append(plugin)
-        logger.success(f"{name}加载成功")
-    else:
-        logger.success(f"{name}已存在")
-
-
-load_plugin("clovers_setu_collection")
+clovers.load_plugin("clovers_setu_collection")
